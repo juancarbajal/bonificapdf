4ec32d7c448a75ccaf440961607d1ae7795334d1
Juan Carbajal <juan.carbajal@redbus.com>
2019-03-29 16:02:17 -0500

    generate pdf file

--- lib/Mail.php
+++ lib/Mail.php
@@ -1,35 +1,48 @@
 <?php
+$config = include(dirname(__FILE__) . '/../config.php');
+require_once dirname(__FILE__).'/dompdf/lib/html5lib/Parser.php';
+//require_once dirname(__FILE__).'/dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+//require_once dirname(__FILE__).'/dompdf/lib/php-svg-lib/src/autoload.php';
+require_once dirname(__FILE__).'/dompdf/src/Autoloader.php';
+Dompdf\Autoloader::register();
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+
+error_reporting(E_ALL&~E_NOTICE);
 /**
  * Generate PDF File from HTML 
  * @param string $template  template name 
- * @param array $values values to replace in template 
- * @return string PDF file url
+ * @param array $keysAndValues keys and values to replace in the template 
+ * @param string $pdfFile filename of PDF generated 
+ * @return integer 0 ok 1: error
  **/
-function generate(string $template, array $values){
+function generatePdf(string $templateFile, array $keysAndValues, string $pdfFile){
+    define('SMARTY_DIR', dirname(__FILE__).'/smarty-3.1.33/libs/');
+    require_once(SMARTY_DIR . 'Smarty.class.php');
+    $smarty = new Smarty();
+    $smarty->setTemplateDir(dirname(__FILE__) . '/../var/template/');
+    $smarty->setCompileDir(dirname(__FILE__) . '/../var/smarty/templates_c/');
+    $smarty->setConfigDir(dirname(__FILE__) . '/../var/smarty/configs/');
+    $smarty->setCacheDir(dirname(__FILE__) . '/../var/smarty/cache/');
+    foreach($keysAndValues as $key=> $value){
+        $smarty->assign($key,$value);
+    }
+    //$smarty->debugging = true;
+    $output = $smarty->fetch('mail-001.tpl');//display
+
+    $options = new Options();
+    $options->set('defaultFont', 'Courier');
+    $dompdf = new Dompdf($options);
+    $dompdf->loadHtml($output);
+    $dompdf->setPaper('A4', 'landscape');
+    $dompdf->render();
+    $outputPDF = $dompdf->output();
+    file_put_contents($pdfFile, $outputPDF);
+    
     return 0;
 }
 
-$config = include(dirname(__FILE__) . '/../config.php');
-//Open db
-//$connection = new PDO("mysql:dbname=" . $config['database']['name'] . ";host=" . $config['database']['host'], $config['database']['user'], //$config['database']['pass']);
-//Read all companies
-//Foreach company read all documents from period (YYYYMM)
-//Create PDFFile
-define('SMARTY_DIR', dirname(__FILE__).'/smarty-3.1.33/libs/');
-require_once(SMARTY_DIR . 'Smarty.class.php');
-$smarty = new Smarty();
-$smarty->setTemplateDir(dirname(__FILE__) . '/../var/template/');
-$smarty->setCompileDir(dirname(__FILE__) . '/../var/smarty/templates_c/');
-$smarty->setConfigDir(dirname(__FILE__) . '/../var/smarty/configs/');
-$smarty->setCacheDir(dirname(__FILE__) . '/../var/smarty/cache/');
-$smarty->assign('companyName','Ned');
-//$smarty->debugging = true;
-$output = $smarty->fetch('mail-001.tpl');//display
-
-require_once dirname(__FILE__).'/dompdf/lib/html5lib/Parser.php';
-require_once dirname(__FILE__).'/dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
-require_once dirname(__FILE__).'/dompdf/lib/php-svg-lib/src/autoload.php';
-require_once dirname(__FILE__).'/dompdf/src/Autoloader.php';
-Dompdf\Autoloader::register();
+generatePdf('mail-001.tpl', array(), 'outputfile.pdf');
 
 //Close db 
